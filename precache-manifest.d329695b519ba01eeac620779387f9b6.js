f51c68761eded01cbf75399c361a8b2763d74b6b
Mariia <maria.bookvoed@gmail.com>
2019-08-06 12:44:35 -0400

    Updates

---
+++ precache-manifest.d329695b519ba01eeac620779387f9b6.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d29e58bc9decf05ed0d9dc42de0c68",
+    "url": "/duchenne-react/index.html"
+  },
+  {
+    "revision": "dbf6281443428d9108c0",
+    "url": "/duchenne-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "a474ca585f4d4788c2e9",
+    "url": "/duchenne-react/static/css/main.40632507.chunk.css"
+  },
+  {
+    "revision": "dbf6281443428d9108c0",
+    "url": "/duchenne-react/static/js/2.f167c37f.chunk.js"
+  },
+  {
+    "revision": "a474ca585f4d4788c2e9",
+    "url": "/duchenne-react/static/js/main.44c612e9.chunk.js"
+  },
+  {
+    "revision": "f57c5164320e3cc0a701",
+    "url": "/duchenne-react/static/js/runtime~main.303678a8.js"
+  },
+  {
+    "revision": "5c5ba2df1f5c289065d3d12a991798df",
+    "url": "/duchenne-react/static/media/Investigacion.5c5ba2df.png"
+  },
+  {
+    "revision": "2110215b4c3536ab57d817474adfba5e",
+    "url": "/duchenne-react/static/media/New-Project(5).2110215b.png"
+  },
+  {
+    "revision": "31d985aeb8fb42d834218048c05c34ec",
+    "url": "/duchenne-react/static/media/New-Project(6).31d985ae.png"
+  },
+  {
+    "revision": "a050ec3ceea04cf77a83e936b6febb2e",
+    "url": "/duchenne-react/static/media/New-Project(7).a050ec3c.png"
+  },
+  {
+    "revision": "18a97443f8b8068c4ddfebcf3c0aeafc",
+    "url": "/duchenne-react/static/media/adn.18a97443.png"
+  },
+  {
+    "revision": "8f9eb2957dee252526a31affe789a5c9",
+    "url": "/duchenne-react/static/media/bienvenido.8f9eb295.jpg"
+  },
+  {
+    "revision": "347b6ecea70f6d67192ed2cc3a65cf27",
+    "url": "/duchenne-react/static/media/donar.347b6ece.jpg"
+  },
+  {
+    "revision": "8903d732f338c23c56c2ffadcc13a38a",
+    "url": "/duchenne-react/static/media/donar.8903d732.png"
+  },
+  {
+    "revision": "b9b441f3fe545ad5529b7fd1452ed50b",
+    "url": "/duchenne-react/static/media/empresa.b9b441f3.svg"
+  },
+  {
+    "revision": "34cb05efa2167eed44da11e7192b8be9",
+    "url": "/duchenne-react/static/media/home-01.34cb05ef.jpg"
+  },
+  {
+    "revision": "66f7d34fa7aa5ee320b98025bc73f930",
+    "url": "/duchenne-react/static/media/home-02.66f7d34f.jpg"
+  },
+  {
+    "revision": "6ef21bbc2ff481f5959c51f6cb5bc129",
+    "url": "/duchenne-react/static/media/iconomontaña.6ef21bbc.jpg"
+  },
+  {
+    "revision": "455ef018b89ecd1790fcf01b1f624f45",
+    "url": "/duchenne-react/static/media/logo.455ef018.jpg"
+  },
+  {
+    "revision": "02ecd5e46b7b0813a7e1bdc5b2a5a0ee",
+    "url": "/duchenne-react/static/media/microdistrofina.02ecd5e4.png"
+  },
+  {
+    "revision": "4c5325d1c92b599f3f9fe2e76a8a916a",
+    "url": "/duchenne-react/static/media/mision.4c5325d1.png"
+  },
+  {
+    "revision": "036dbe1dc15b201b31951f50dff94113",
+    "url": "/duchenne-react/static/media/niño.036dbe1d.jpg"
+  },
+  {
+    "revision": "480daec0194b93cf9594a03cdc62917d",
+    "url": "/duchenne-react/static/media/paleta.480daec0.png"
+  },
+  {
+    "revision": "ef9250b8cb5e74fc6d4f01001f8d6840",
+    "url": "/duchenne-react/static/media/superhombre.ef9250b8.svg"
+  }
+]);
